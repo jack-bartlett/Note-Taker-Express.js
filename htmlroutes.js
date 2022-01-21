c05b380aff6393a9f66c03b272a515fd0d8740f1
Jack Bartlett <90886034+jack-bartlett@users.noreply.github.com>
2022-01-21 17:47:26 -0500

    case sensitive errors corrected v1

--- htmlroutes.js
+++ htmlroutes.js
@@ -5,13 +5,13 @@ const path = require('path');
 // Get route for homepage
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 // Get route for notes page
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-  module.exports = router;
+module.exports = router;
